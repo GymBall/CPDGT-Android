3efe49d6c32530a8b5d9b1487d86d31e00cc332e
nyongja <nyongja@naver.com>
2019-11-14 23:19:11 +0900

    fragment_home(incomplete)

--- app/src/main/java/org/gymball/cpdgt_android/ui/home/HomeFragment.kt
+++ app/src/main/java/org/gymball/cpdgt_android/ui/home/HomeFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.SnapHelper
+import kotlinx.android.synthetic.main.fragment_home.*
 
 import org.gymball.cpdgt_android.R
 
@@ -13,6 +16,8 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
