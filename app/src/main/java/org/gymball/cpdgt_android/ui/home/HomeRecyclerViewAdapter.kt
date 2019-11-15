5c4386c83674a2d82aabc7bb708be3c332c5bb2b
nyongja <nyongja@naver.com>
2019-11-15 20:00:26 +0900

    fragment_home(RecyclerView)

--- app/src/main/java/org/gymball/cpdgt_android/ui/home/HomeRecyclerViewAdapter.kt
+++ app/src/main/java/org/gymball/cpdgt_android/ui/home/HomeRecyclerViewAdapter.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.rv_home_image.view.*
 import org.gymball.cpdgt_android.R
 import org.jetbrains.anko.image
 
