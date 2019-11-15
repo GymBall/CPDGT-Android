3b298c2de847b254d8717773cc5dfe335a9e4b9d
nyong <nyongja@naver.com>
2019-11-15 15:54:52 +0900

    fragment_home(recycler_View)

--- app/src/main/java/org/gymball/cpdgt_android/ui/home/HomeFragment.kt
+++ app/src/main/java/org/gymball/cpdgt_android/ui/home/HomeFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.SnapHelper
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -13,13 +15,29 @@ import kotlinx.android.synthetic.main.fragment_home.*
 import org.gymball.cpdgt_android.R
 
 class HomeFragment : Fragment() {
+    lateinit var HomeRecyclerViewAdapter : HomeRecyclerViewAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
-
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView(){
+        var dataList : ArrayList<HomeImageData> = ArrayList()
+        dataList.add(HomeImageData(getResources().getDrawable(R.drawable.home_image1)!!))
+        dataList.add(HomeImageData(getResources().getDrawable(R.drawable.home_image2)!!))
+        dataList.add(HomeImageData(getResources().getDrawable(R.drawable.home_image3)!!))
+
+        HomeRecyclerViewAdapter = HomeRecyclerViewAdapter(activity!!, dataList)
+        rv_home_image.adapter = HomeRecyclerViewAdapter
+        rv_home_image.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+    }
 
 }
---
+++ app/src/main/java/org/gymball/cpdgt_android/ui/home/HomeRecyclerViewAdapter.kt
@@ -0,0 +1,29 @@
+package org.gymball.cpdgt_android.ui.home
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.rv_home_image.view.*
+import org.gymball.cpdgt_android.R
+import org.jetbrains.anko.image
+
+class HomeRecyclerViewAdapter (val ctx : Context, val dataList : ArrayList<HomeImageData>) : RecyclerView.Adapter<HomeRecyclerViewAdapter.Holder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view : View = LayoutInflater.from(ctx).inflate(R.layout.rv_home_image, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int = dataList.size
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.homeimage.image = dataList[position].homeimage
+
+    }
+    inner class Holder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val homeimage : ImageView = itemView.findViewById(R.id.home_image) as ImageView
+    }
+}
