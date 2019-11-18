452ade81032482348eb9edb532ada52fe5e2b17f
eujuu <e97214j@naver.com>
2019-11-18 23:20:23 +0900

    20191118 modify

---
+++ app/src/main/java/org/gymball/cpdgt_android/ui/gtlecture/Lecture.kt
@@ -0,0 +1,3 @@
+package org.gymball.cpdgt_android.ui.gtlecture
+
+class Lecture (val lecturename: String, val lecturetime: String, val lecturegrade: String, val totalcount: String, val waitcount: String, val photo: String)
