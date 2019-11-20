92c0811214fcbf76a30e89b092e1ffc55313f668
eumiing <wjdma0103@naver.com>
2019-11-20 17:28:53 +0900

    jeongeum 최종

--- app/src/main/java/org/gymball/cpdgt_android/ui/lecture/LectureRecyclerViewAdapter.kt
+++ app/src/main/java/org/gymball/cpdgt_android/ui/lecture/LectureRecyclerViewAdapter.kt
@@ -9,11 +9,9 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.fragment_lecture.view.*
 import org.gymball.cpdgt_android.R
 import org.gymball.cpdgt_android.model.LectureListData
 import org.gymball.cpdgt_android.ui.lecturedetail.LectureDetailActivity
-import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 
 class LectureRecyclerViewAdapter(val ctx:Context, var dataList: ArrayList<LectureListData>, var total: Int) : RecyclerView.Adapter<LectureRecyclerViewAdapter.Holder>() {
