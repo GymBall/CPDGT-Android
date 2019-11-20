7f90f1a60ea5c423199acffb6cb86de79747ac99
LAPTOP-0AA7JK7P\bys04 <bys04026@gmail.com>
2019-11-20 12:40:35 +0900

    login, myGym 화면전환 및 recyclerview 구현 login, fragment_my, rv_item_my_recent 폰트수정 fragment_my 레이아웃 수정

--- app/src/main/java/org/gymball/cpdgt_android/ui/login/LoginActivity.kt
+++ app/src/main/java/org/gymball/cpdgt_android/ui/login/LoginActivity.kt
@@ -2,12 +2,30 @@ package org.gymball.cpdgt_android.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 import org.gymball.cpdgt_android.R
+import org.gymball.cpdgt_android.ui.MainActivity
+import org.gymball.cpdgt_android.ui.join.JoinActivity
+import org.jetbrains.anko.startActivity
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        loginButton.setOnClickListener {
+            startActivity<MainActivity>()
+        }
+        GTJoinButton.setOnClickListener {
+            startActivity<JoinActivity>() //짐선생 조인으로 수정하기
+        }
+        GSJoinButton.setOnClickListener {
+            startActivity<JoinActivity>() //짐학생 조인으로 수정하기
+        }
     }
 }
--- app/src/main/java/org/gymball/cpdgt_android/ui/my/MyFragment.kt
+++ app/src/main/java/org/gymball/cpdgt_android/ui/my/MyFragment.kt
@@ -6,15 +6,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_my.*
 
 import org.gymball.cpdgt_android.R
+import org.gymball.cpdgt_android.model.LectureAbsData
+import org.gymball.cpdgt_android.ui.login.LoginActivity
+import org.jetbrains.anko.support.v4.startActivity
 
 class MyFragment : Fragment() {
+
+    lateinit var myRecyclerViewAdapter: MyRecyclerViewAdapter
+    var dataList: ArrayList<LectureAbsData> = arrayListOf(
+        LectureAbsData("클래스 이름", "클래스 시간대", R.drawable.lecture_comment_teacher,"뫄뫄"),
+        LectureAbsData("클래스 이름", "클래스 시간대", R.drawable.lecture_comment_teacher,"묘묘"),
+        LectureAbsData("클래스 이름", "클래스 시간대", R.drawable.lecture_comment_teacher,"뮤뮤")
+    )
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setRecyclerView()
+        setOnClickListener()
+    }
 
+    private fun setRecyclerView() {
+        myRecyclerViewAdapter = MyRecyclerViewAdapter(activity!!, dataList, dataList.size)
+        rv_my_recent.adapter = myRecyclerViewAdapter
+        rv_my_recent.layoutManager = LinearLayoutManager(activity!!, LinearLayoutManager.HORIZONTAL, false)
+    }
+    private fun setOnClickListener() {
+        loginInfo.setOnClickListener {
+            startActivity<LoginActivity>()
+        }
+    }
 }
